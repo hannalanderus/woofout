3750b7ffc4261d46667f0ba3bd080e3715a5ca88
dbaettig <daniela.baettig@hotmail.com>
2018-11-23 14:20:09 +0100

    firebase

--- src/components/App.js
+++ src/components/App.js
@@ -10,51 +10,51 @@ import fire from './config/Fire';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
-  
-  constructor(props){
+
+  constructor(props) {
     super(props);
-    this.state ={
-      user:{},
+    this.state = {
+      user: {},
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.authListener();
   }
-  
 
-  authListener(){
-  fire.auth().onAuthStateChanged((user) => {
-    console.log(user);
-    if (user){
-      this.setState({ user });
-      localStorage.setItem('user', user.uid);
-    } else {
-      this.setState({ user: null });
-      localStorage.removeItem('user');
-    }
-  });
-}
+
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      if (user) {
+        this.setState({ user });
+        localStorage.setItem('user', user.uid);
+      } else {
+        this.setState({ user: null });
+        localStorage.removeItem('user');
+      }
+    });
+  }
 
 
 
   render() {
 
     return (
-     <div>
-     {this.state.user ? (<Profile/>) : (< StartPage/>)}
-     <HamburgerMenu/>
-      <BrowserRouter>
       <div>
-          <Switch>
-            <Route path="/" component={StartPage} exact/>
-            <Route path="/Registration" component={Registration} />
-            <Route path="/Profile" component={Profile} />
-            <Route path="/WorkoutBank" component={WorkoutBank} />
-            <Route path="/RegistrationDog" component={RegistrationDog} />
-          </Switch>
-      </div>
-       </BrowserRouter> 
+        {this.state.user ? (<Profile />) : (< StartPage />)}
+        <HamburgerMenu />
+        <BrowserRouter>
+          <div>
+            <Switch>
+              <Route path="/" component={StartPage} exact />
+              <Route path="/Registration" component={Registration} />
+              <Route path="/Profile" component={Profile} />
+              <Route path="/WorkoutBank" component={WorkoutBank} />
+              <Route path="/RegistrationDog" component={RegistrationDog} />
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
