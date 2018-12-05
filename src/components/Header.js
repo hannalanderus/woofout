10d9028232651876e5f31c1dd62cf5ac7ac7ccee
Hanna Landerus <you@example.com>
2018-12-05 16:43:02 +0100

    break out header, header component styling

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../resources/scss/style.scss';
+
+
+const Header = () => {
+
+    return (
+      <div className="StartPage-header">
+            <h1 className="woofout">Woofout</h1>
+            <h1 className="registartionHeadline">Registrering</h1>
+            <h1 className="profileHeadline">Profil</h1>
+            <h1 className="dogs">Dina hundar</h1>
+      </div>
+      
+    );
+
+}
+
+
+export default Header;
