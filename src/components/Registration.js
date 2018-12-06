0f44ed7a08bc8e7d56493aad775c366e08d9752c
dbaettig <daniela.baettig@hotmail.com>
2018-12-06 18:08:35 +0100

    styled registration page for better oversight

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -11,35 +11,35 @@ class Registration extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.signup = this.signup.bind(this);
     this.state = {
-      name:'',
-      surname:'',
+      name: '',
+      surname: '',
       email: '',
       password: '',
     }
-             }
+  }
 
   signup(e) {
     e.preventDefault();
     var db = firebase.firestore();
-  
+
     firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u) => {
       var user = firebase.auth().currentUser;
       console.log(user);
-        db.collection("users").doc(user.uid).set({
-          name: this.state.name,
-          surname: this.state.surname,
+      db.collection("users").doc(user.uid).set({
+        name: this.state.name,
+        surname: this.state.surname,
+      })
+      user.sendEmailVerification().then(function () {
+        console.log("verification email sent");
+      }).then(function () {
+        alert("Document successfully written!");
+        window.location.href = "/RegistrationDog";
+      }).catch(function (error) {
+        alert(error);
+      });
     })
-        user.sendEmailVerification().then(function() { 
-         console.log("verification email sent"); 
-         }).then(function() {
-           alert("Document successfully written!");
-           window.location.href="/RegistrationDog";
-           }).catch(function(error){ 
-            alert(error); 
-             });
-  })
 
-}
+  }
 
   handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
@@ -49,17 +49,19 @@ class Registration extends Component {
   render() {
     return (
       <div className="App-registration">
-        < Header/>
-        <div className="App-header-registration blue-gradient">
-          <div className="inputWrapper ">
-            <input value={this.state.name} onChange={this.handleChange} type="name" name="name" id="name" placeholder="Förnamn"></input>
-            <input value={this.state.surname} onChange={this.handleChange} type="surname" name="surname" id="surname" placeholder="Efternamn"></input>
-            <input value={this.state.email} onChange={this.handleChange} type="email" name="email" id="email" placeholder="Email"></input>
-            <input value={this.state.password} onChange={this.handleChange} type="password" name="password" id="password" placeholder="Lösenord"></input>
-            <input placeholder="Konfirmera lösenord"></input>
-            <button onClick={this.signup} className="button" id="signUp">Registrera</button>
+        < Header />
+        <section class="RegistrationPage">
+          <div className="RegistrationPage-wrapper">
+            <div className="RegistrationPage-form">
+              <input value={this.state.name} onChange={this.handleChange} type="name" name="name" id="name" placeholder="Förnamn"></input>
+              <input value={this.state.surname} onChange={this.handleChange} type="surname" name="surname" id="surname" placeholder="Efternamn"></input>
+              <input value={this.state.email} onChange={this.handleChange} type="email" name="email" id="email" placeholder="Email"></input>
+              <input value={this.state.password} onChange={this.handleChange} type="password" name="password" id="password" placeholder="Lösenord"></input>
+              <input placeholder="Konfirmera lösenord"></input>
+              <button onClick={this.signup} className="regUserButton" id="signUp">Registrera</button>
+            </div>
           </div>
-        </div>
+        </section>
       </div >
     );
   }
