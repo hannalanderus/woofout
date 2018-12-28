589151a4609eb42f4b8297e51965aa8ba0a4f2b6
Hanna Landerus <you@example.com>
2018-12-28 15:49:04 +0100

    adjust filer - workoutBank

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -6,58 +6,18 @@ import '../resources/scss/style.scss';
 
 class Filter extends Component {
 
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        //this.filterResults = this.filterResults.bind(this);
-        this.state = {
-            name: '',
-            //data: []
-        }
-    }
-
-
-    handleChange = (val) => {
-        this.setState({name : val});
-        //this.setState({ [e.target.name]: e.target.value });
-        console.log(this.state.name)
-    }
-
-    // filterResults(e) {
-    //     const current = this.state.name
-    //     console.log(this.state.name);
-
-    //     fire.firestore().collection('trainingprogram').where("category", "==", current)
-    //         .get()
-    //         .then((querySnapshot) => {
-    //             const data = [];
-    //             querySnapshot.forEach((doc) => {
-    //                 const { name, category, purpose, material, description } = doc.data();
-    //                 data.push({
-    //                     id: doc.id,
-    //                     doc, // DocumentSnapshot
-    //                     name,
-    //                     category,
-    //                     purpose,
-    //                     material,
-    //                     description,
-
-    //                 });
-    //                 //console.log(this.state.image);
-    //             });
-    //             this.setState({ data });
-    //         })
-    // }
-
 
     render() {
 
         return (
             <div>
-                {/*<button onClick={this.filterResults} className="greyButton" id="signUp">Sök</button>*/}
-                <li onClick={this.handleChange.bind(this, "Rörlighet")}>Rörlighet</li>
-                <li onClick={this.handleChange.bind(this, "Balans")}>Balans</li>
-                <li onClick={this.handleChange.bind(this, "Stabilitet")}>Stabilit</li>        
+                 <ul className="workoutPage-list" key={this.props.id}>
+                    <li className="listName">{this.props.name}</li>
+                    <li className="listType">{this.props.category}</li>
+                    <li>Syfte:{this.props.purpose}</li>
+                    <li>Material:{this.props.material}</li>
+                    <li>Beskrivning: {this.props.description}</li>
+                   </ul>          
             </div>
         )
     }
