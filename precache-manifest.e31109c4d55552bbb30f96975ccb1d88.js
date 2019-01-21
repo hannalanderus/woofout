d108e605e45387fbe5201c06dc0968b5bda1caea
dbaettig <daniela.baettig@hotmail.com>
2019-01-21 15:39:51 +0100

    Updates

---
+++ precache-manifest.e31109c4d55552bbb30f96975ccb1d88.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "87e5f3eb2902c8ad09635d66592aa484",
+    "url": "/woofout/static/media/avatar-inside-a-circle.87e5f3eb.svg"
+  },
+  {
+    "revision": "1c1647360dbd60513e6a",
+    "url": "/woofout/static/css/main.4cbae291.chunk.css"
+  },
+  {
+    "revision": "bebc8657a981f6818da5",
+    "url": "/woofout/static/js/1.bebc8657.chunk.js"
+  },
+  {
+    "revision": "5b5c1810d89f1f366b80",
+    "url": "/woofout/static/js/runtime~main.5b5c1810.js"
+  },
+  {
+    "revision": "47a6273387f33cbbed0e62655f346bcd",
+    "url": "/woofout/static/media/pawprint-outline-in-a-circle.47a62733.svg"
+  },
+  {
+    "revision": "767a42e129578cae0e9ce2bc000d2f24",
+    "url": "/woofout/static/media/logout.767a42e1.svg"
+  },
+  {
+    "revision": "1c1647360dbd60513e6a",
+    "url": "/woofout/static/js/main.1c164736.chunk.js"
+  },
+  {
+    "revision": "ed718a5fe4c2c476c20d3e21a9f22868",
+    "url": "/woofout/static/media/plus.ed718a5f.svg"
+  },
+  {
+    "revision": "be5652dd600774b4b59bbfb8e480e1d3",
+    "url": "/woofout/static/media/badge.be5652dd.svg"
+  },
+  {
+    "revision": "c4f1d854077edbc0b000e19256f2ec34",
+    "url": "/woofout/static/media/whistle.c4f1d854.svg"
+  },
+  {
+    "revision": "1d8fda1e59f8bc9bd506cc4fbdcacf9c",
+    "url": "/woofout/static/media/photo.1d8fda1e.svg"
+  },
+  {
+    "revision": "60a2962a0226afae5801f0e30d53545f",
+    "url": "/woofout/index.html"
+  }
+];
